376c98d351d92fcf37cff38bf0d3e2588d19cc86
Steven Dunn <thevgm01@gmail.com>
2019-11-07 02:41:13 -0600

    set up the game world and created textures

---
+++ Assets/CameraRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float rotSpeed = 1f;
+    float rot = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, rot, 0);
+        rot += rotSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/CharacterSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour
+{
+    [SerializeField] List<Material> characterMats;
+    public GameObject characterObject;
+    public Transform podiums;
+
+    List<GameObject> characters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characters = new List<GameObject>();
+        int i = 0;
+        foreach(Transform podium in podiums) {
+            foreach(Transform realPodium in podium) {
+                foreach (Transform child in realPodium) {
+                    if (child.name == "PlaceToStand") {
+                        GameObject newChar = Instantiate(characterObject, child);
+                        foreach(Transform sprite in newChar.transform) {
+                            sprite.GetComponent<MeshRenderer>().material = characterMats[i];
+                        }
+                        characters.Add(newChar);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
