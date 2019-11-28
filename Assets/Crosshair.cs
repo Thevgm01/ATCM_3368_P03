cf8a99e2841c4531e53d78c8965ccafc33a3c1a7
Thevgm01 <thevgm01@gmail.com>
2019-11-28 00:08:11 -0700

    added the crosshair and created the framework for the debate text

---
+++ Assets/Crosshair.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public float minCrosshairSpinSpeed,
+                 maxCrosshairSpinSpeed;
+    float curCrosshairAngle = 0.0f;
+
+    Transform inner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            if (child.name == "Inner") inner = child;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;
+
+        if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            curCrosshairAngle += maxCrosshairSpinSpeed * Time.deltaTime;
+        else 
+            curCrosshairAngle += minCrosshairSpinSpeed * Time.deltaTime;
+
+        inner.rotation = Quaternion.Euler(0, 0, -curCrosshairAngle);
+    }
+}
---
+++ Assets/NonstopDebateText.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NonstopDebateText : MonoBehaviour
+{
+    public Vector3 prePosition,
+                   preRotation,
+                   startPosition,
+                   startRotation,
+                   forceOverTime,
+                   rotationOverTime;
+
+    public float preDuration,
+                 duration,
+                 postDuration;
+
+    float awakeTimer;
+
+    TextMeshPro[] textMeshes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshes = GetComponentsInChildren<TextMeshPro>();
+
+        duration += preDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(awakeTimer < preDuration)
+        {
+            float fraction = awakeTimer / preDuration;
+            transform.localPosition = Vector3.Lerp(prePosition, startPosition, fraction);
+            transform.localRotation = Quaternion.Euler(Vector3.Lerp(preRotation, startRotation, fraction));
+            foreach(var tm in textMeshes)
+                tm.alpha = fraction;
+        }
+        else if(awakeTimer < duration)
+        {
+            transform.localPosition = startPosition - forceOverTime * (awakeTimer - preDuration);
+            transform.localRotation = Quaternion.Euler(startRotation - rotationOverTime * (awakeTimer - preDuration));
+        }
+
+        if(awakeTimer > duration)
+        {
+            float fraction = 1.0f - (awakeTimer - duration) / postDuration;
+            foreach (var tm in textMeshes)
+                tm.alpha = fraction;
+        }
+
+        awakeTimer += Time.deltaTime;
+    }
+}
